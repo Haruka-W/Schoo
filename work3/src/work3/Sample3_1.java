a50ec759cb5d9512b542d83abfd6274e428a7cbf
Haruka W <nekonoko1995@gmail.com>
2020-06-28 19:18:49 +0900

    [Start]Java入門初級ー第３回

---
+++ work3/src/work3/Sample3_1.java
@@ -0,0 +1,9 @@
+package work3;
+
+public class Sample3_1 {
+
+	public static void main(String[] arcs) {
+		int x = 100;
+		System.out.println("変数xの値は" + x);
+	}
+}
---
+++ work3/src/work3/Sample3_2.java
@@ -0,0 +1,11 @@
+package work3;
+
+public class Sample3_2 {
+
+	public static void main(String[] arcs) {
+		int x = 100;
+		int y = 200;
+		System.out.println("変数xの値は" + x);
+		System.out.println("変数yの値は" + y);
+	}
+}
