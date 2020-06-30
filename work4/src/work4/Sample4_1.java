ddd05f3009c6de183a089ebc5ff3a13cc898e1fd
Haruka W <nekonoko1995@gmail.com>
2020-06-30 16:08:31 +0900

    [Start]Java入門初級ー第４回

---
+++ work4/src/work4/Sample4_1.java
@@ -0,0 +1,23 @@
+package work4;
+
+public class Sample4_1 {
+
+	public static void main(String[] arcs) {
+		int x = 10;
+		if(x >= 10) {
+			System.out.println("Hello!");
+		}
+
+		double y = 50;
+		if(y < 100) {
+			System.out.println("Hello!");
+		}
+
+		int z = 10;
+		if(z == x) {
+			System.out.println("Hekko!");
+		}
+	}
+
+
+}
