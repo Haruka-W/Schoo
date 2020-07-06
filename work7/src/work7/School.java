a1a3d68dbb8b820c0ec07f99591cba0a4a959afa
Haruka W <nekonoko1995@gmail.com>
2020-07-06 17:41:23 +0900

    [Start]Java入門初級ー第７回

---
+++ work7/src/work7/School.java
@@ -0,0 +1,16 @@
+package work7;
+
+public class School {
+
+	public static void main(String[] args) {
+		// インスタンス化
+		Student s = new Student();
+		// 自己紹介（1回目）
+		s.introduction();
+		// 年齢の計算
+		s.ageCalculation();
+		// 自己紹介（2回目）
+		s.introduction();
+	}
+
+}
